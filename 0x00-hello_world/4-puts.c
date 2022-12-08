439c02178c84f8f68e7ce546a88ca9a5ae67ca2d
KingPanda68 <kingsleyonyenze68@gmail.com>
2022-12-08 13:05:35 -0800

    correction - Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/*
+/**
 * main - Entry point
 *
 * Description: prints a string
 *
-* * Return: Always 0
+* Return: Always 0
 */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
