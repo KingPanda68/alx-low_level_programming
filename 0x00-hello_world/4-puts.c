d73b52edac40e0d1f1361f76a631ee75364a1e53
KingPanda68 <kingsleyonyenze68@gmail.com>
2022-12-08 11:27:55 -0800

    correction - Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-# i/ bin / bash
 #include <stdio.h>
 
 /*
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+* main - Entry point
+*
+* * Return: Always 0 (Success)
 */
 int main(void)
 {
