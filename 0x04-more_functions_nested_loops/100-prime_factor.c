1722b3628e3d92b35a96e3446d04837223f3ece3
KingPanda68 <kingsleyonyenze68@gmail.com>
2022-12-16 13:04:24 -0800

    correcion

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,5 +6,24 @@
 */
 int main(void)
 {
+	long input = 612852475143;
+	int iter;
+
+	while (iter++ < (input / 2))
+	{
+		if ((input % iter) == 0)
+		{
+			input /= 2;
+			continue;
+		}
+
+		for (iter = 3; iter < (input / 2); iter += 2)
+		{
+			if ((input % iter) == 0)
+				input /= iter;
+		}
+	}
+
+	printf("%ld\n", input);
 	return (0);
 }
